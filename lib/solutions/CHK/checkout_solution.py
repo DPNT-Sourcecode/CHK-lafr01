61fbb4826d418b7671398711e8afcb7d7fdb4747
sbx_user1051 <sbx_user1051@169.254.58.249>
2022-05-18 12:05:20 +0000

    Wed May 18 12:05:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,9 +3,14 @@ from collections import defaultdict
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    items = defaultdict(lambda: 0)
+    items = { 
+        "A": {"price": 50, "count": 0},
+        "B": {"price": 50, "count": 0},
+        "C": {"price": 50, "count": 0},
+        "D": {"price": 50, "count": 0} }
     for item in skus:
         items[item] += 1
     
     raise NotImplementedError()
+
 
