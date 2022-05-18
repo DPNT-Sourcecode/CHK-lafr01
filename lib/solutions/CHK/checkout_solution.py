3344d888858404de4ba8c3059d6c6c27c9d744c0
sbx_user1051 <sbx_user1051@169.254.58.249>
2022-05-18 12:02:20 +0000

    Wed May 18 12:02:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,11 @@
-
+from collections import defaultdict
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    items = defaultdict(lambda: 0)
+    for item in skus:
+        items[item] += 1
+    
     raise NotImplementedError()
+
